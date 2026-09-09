b53b44ed2fa9c0bdb1126b9884b98f2032d5c6af
FadhilR12 <fadhilrahman0814@gmail.com>
2026-09-09 15:57:09 +0700

    add adapter node

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto'; //diganti dengan node
+import adapter from '@sveltejs/adapter-node'; //diganti dengan node
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
